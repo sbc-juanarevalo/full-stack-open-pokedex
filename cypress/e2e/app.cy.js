6d05afcafd2c20a8d6c946dfbb318ecea3e59c89
sbc-juanarevalo <juanarevalo@subocol.com>
2023-05-28 10:10:19 -0500

    Commit 28

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -3,7 +3,7 @@
 describe('Pokedex',function(){
   it('frontpage can be opened', function(){
     cy.visit('http://localhost:5000')
-    cy.contains('pichachu')
+    cy.contains('pikachu')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
   })
 })
